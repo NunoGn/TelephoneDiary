84ed4985bacc7ee42d56b6790099d972547cbaf1
Nuno Correia <nunogncorreia25@gmail.com>
2020-10-13 14:49:26 +0100

    Telephone Diary

---
+++ TelephoneDiary/TelephoneDiary/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace TelephoneDiary
+{
+    partial class Contacts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblMobile = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.btNew = new System.Windows.Forms.Button();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(47, 43);
+            this.lblFirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(95, 20);
+            this.lblFirstName.TabIndex = 7;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(50, 83);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(92, 20);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblMobile
+            // 
+            this.lblMobile.AutoSize = true;
+            this.lblMobile.Location = new System.Drawing.Point(80, 123);
+            this.lblMobile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMobile.Name = "lblMobile";
+            this.lblMobile.Size = new System.Drawing.Size(62, 20);
+            this.lblMobile.TabIndex = 9;
+            this.lblMobile.Text = "Mobile";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(88, 163);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(54, 20);
+            this.lblEmail.TabIndex = 10;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(63, 202);
+            this.lblCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(79, 20);
+            this.lblCategory.TabIndex = 11;
+            this.lblCategory.Text = "Category";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(150, 43);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(347, 26);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(150, 83);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(347, 26);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(150, 123);
+            this.txtMobile.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(347, 26);
+            this.txtMobile.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(150, 163);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(347, 26);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Items.AddRange(new object[] {
+            "Home",
+            "Office",
+            "Business",
+            "Friends",
+            "Family"});
+            this.cbxCategory.Location = new System.Drawing.Point(150, 201);
+            this.cbxCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(198, 28);
+            this.cbxCategory.TabIndex = 4;
+            // 
+            // btNew
+            // 
+            this.btNew.Location = new System.Drawing.Point(601, 35);
+            this.btNew.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btNew.Name = "btNew";
+            this.btNew.Size = new System.Drawing.Size(114, 45);
+            this.btNew.TabIndex = 5;
+            this.btNew.Text = "New";
+            this.btNew.UseVisualStyleBackColor = true;
+            this.btNew.Click += new System.EventHandler(this.btNew_Click);
+            // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(601, 84);
+            this.btInsert.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(114, 45);
+            this.btInsert.TabIndex = 6;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView.Location = new System.Drawing.Point(23, 300);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(692, 221);
+            this.dataGridView.TabIndex = 12;
+            this.dataGridView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView_MouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "First Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Last Name";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Mobile";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Email";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Category";
+            this.Column5.Name = "Column5";
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(601, 190);
+            this.btUpdate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(114, 45);
+            this.btUpdate.TabIndex = 14;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(601, 136);
+            this.btDelete.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(114, 45);
+            this.btDelete.TabIndex = 13;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(452, 265);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(263, 26);
+            this.txtSearch.TabIndex = 15;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Contacts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 533);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.btNew);
+            this.Controls.Add(this.cbxCategory);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblMobile);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Contacts";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telephone Diary";
+            this.Load += new System.EventHandler(this.Contacts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblMobile;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.Button btNew;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ TelephoneDiary/TelephoneDiary/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TelephoneDiary
+{
+
+
+    public partial class Contacts : Form
+    {
+
+        SqlConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Contacts;Integrated Security=True");
+
+        public Contacts()
+
+        {
+            InitializeComponent();
+        }
+        private void Contacts_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void btNew_Click(object sender, EventArgs e) //button1
+        {
+            txtFirstName.Text = "";
+            txtLastName.Clear();
+            txtMobile.Text = "";
+            txtEmail.Clear();
+            cbxCategory.SelectedIndex = -1;
+            txtFirstName.Focus();
+        }
+
+        private void btInsert_Click(object sender, EventArgs e) //button2
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO Contact (FirstName,LastName,Mobile,Email,Category)
+                        values('" + txtFirstName.Text + "','" + txtLastName.Text + "','" + txtMobile.Text + "','" + txtEmail.Text + "','" + cbxCategory.Text + "')", connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Successfully Saved!");
+            Display();
+        }
+
+        void Display()
+        {
+            SqlDataAdapter adapt = new SqlDataAdapter("Select * from Contact", connection);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView.Rows.Add();
+                dataGridView.Rows[n].Cells[0].Value = item["FirstName"].ToString();
+                dataGridView.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView.Rows[n].Cells[4].Value = item[4].ToString();
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM Contact WHERE(Mobile='" + txtMobile.Text + "')", connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Delete Successfully!");
+            Display();
+
+        }
+
+        private void dataGridView_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtFirstName.Text = dataGridView.SelectedRows[0].Cells[0].Value.ToString();
+            txtLastName.Text = dataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            txtMobile.Text = dataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            txtEmail.Text = dataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            cbxCategory.Text = dataGridView.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(@"UPDATE Contact
+                                    SET FirstName='" + txtFirstName.Text + "', LastName='" + txtLastName.Text + "'," +
+                                    "Mobile='" + txtMobile.Text + "',Email='" + txtEmail.Text + "',Category='" + cbxCategory.Text + "'" +
+                                    " WHERE (Mobile='" + txtMobile.Text + "')", connection);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Update Successfully!");
+            Display();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapt = new SqlDataAdapter("Select * from Contact WHERE Mobile LIKE '" + txtSearch.Text + "%' OR FirstName LIKE '%" + txtSearch.Text + "%'", connection);
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView.Rows.Add();
+                dataGridView.Rows[n].Cells[0].Value = item["FirstName"].ToString();
+                dataGridView.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView.Rows[n].Cells[4].Value = item[4].ToString();
+            }
+        }
+    }
+}
+
+
